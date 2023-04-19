ca799d10cb61a96f39f3b8569a6c5bc5c16388bc
muxin <50224473+chuxinshuo@users.noreply.github.com>
2023-04-19 16:45:12 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'kafeiyucha@126.com',
   link: 'https://www.muxinji.com',
   newsletter: 'Notionic Weekly',
-  description: 'Notionic, A static blog build on top of Notion and Next.js',
+  description: 'Notionic, A static blog build on top of Notion and ',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
