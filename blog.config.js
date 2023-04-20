21752dd281eb1a6a0a27b87cce3861fe26cc631d
muxin <50224473+chuxinshuo@users.noreply.github.com>
2023-04-20 08:23:40 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'kafeiyucha@126.com',
   link: 'https://www.muxinji.com',
   newsletter: 'Notionic Weekly',
-  description: '以心为畜，用爱视之，使之平静安宁',
+  description: 'Use the heart as a guide, view with love, and bring peace and tranquility to it.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
