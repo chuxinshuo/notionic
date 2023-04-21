19bbe7a16aa9ce7973757932ddaaaf7d612dd09a
muxin <50224473+chuxinshuo@users.noreply.github.com>
2023-04-21 15:03:24 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -29,7 +29,7 @@ const BLOG = {
   ogImageGenerateHost: 'og-zl.vercel.app', // The link to generate OG image, don't end with a slash
   defaultCover: '/cover.jpg',
   socialLink: {
-    twitter: 'https://twitter.com/izuolan',
+    twitter: '/cover.jpg',
     github: 'https://github.com/chuxinshuo',
     telegram: 'https://t.me/muxinji'
   },
